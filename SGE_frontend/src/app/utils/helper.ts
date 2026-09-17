1275cba0cea0cf7c6dd85acc53c4d486b919adc2
DuHuDiCo <duvadico@suramericana.com.co>
2026-09-16 20:35:55 -0500

    c

--- SGE_frontend/src/app/utils/helper.ts
+++ SGE_frontend/src/app/utils/helper.ts
@@ -1,6 +1,6 @@
 // let baseUrl = "http://api-gatewa-microservice.duckdns.org/api/v1";
 
-let baseUrl = "https://api-gatewa-microservice.duckdns.org/api/v1";
+let baseUrl = "https://gjmhogar.io/backend/api/v1";
 
 //let baseUrl = "http://192.168.1.241:9000/api/v1";
 
